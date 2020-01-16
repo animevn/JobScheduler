ada2ed9b9ca24847082b4d6bbb0192ba0ba39f3f
animevn <haanhth21@gmail.com>
2020-01-16 08:26:29 +0700

    add job service

---
+++ app/src/main/java/com/haanhgs/jobschedulerdemo/Job.java
@@ -0,0 +1,29 @@
+package com.haanhgs.jobschedulerdemo;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+
+/*
+add these lines to manifest
+<service
+     android:name=".Jobs"
+     android:permission="android.permission.BIND_JOB_SERVICE">
+</service>
+*/
+
+public class Job extends JobService {
+
+    public static final int ID = 1979;
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        Notification notification = new Notification(this);
+        notification.createNotification(1979);
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+}
